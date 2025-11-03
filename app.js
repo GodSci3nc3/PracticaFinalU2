8703db0b09c171db031500478cfe1a63887bc5d0
Arturo Rosales V <rosalesvelazquezarturo@gmail.com>
2025-11-03 07:26:10 -0600

    Products alignment

---
+++ app.js
@@ -0,0 +1,5 @@
+document.getElementById("add-to-cart").addEventListener("click", function() {
+    // Guardar en un array el producto añadido al carrito
+    let cart = [];
+    cart.push("Tortitas de Huazontle");
+}   );
